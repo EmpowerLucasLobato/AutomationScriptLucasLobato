3b1d6becd00cf54d0568e41106e36f952783b865
llobato12 <135057463+llobato12@users.noreply.github.com>
2023-05-30 16:00:27 -0300

    Change

--- EmpowerLucas_1/EmpowerLucas_1.cs
+++ EmpowerLucas_1/EmpowerLucas_1.cs
@@ -68,7 +68,8 @@ namespace EmpowerLucas_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello word"); 
+			engine.GenerateInformation("Hello word");
+			// Change
 		}
 	}
 }
