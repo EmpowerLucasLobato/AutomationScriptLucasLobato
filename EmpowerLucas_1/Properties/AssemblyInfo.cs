89263eef47328bae52a923fd02a740e3389ef9fa
llobato12 <135057463+llobato12@users.noreply.github.com>
2023-05-30 15:54:37 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerLucas_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerLucas_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerLucas_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9AA64E7E-8466-48C1-9B1A-950B06E35236")]
+[assembly: AssemblyVersion("1.0.0.0")]
